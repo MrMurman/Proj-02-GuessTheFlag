6ac1cc69673e9ee3f1ab83f4f6a774b9706a86c0
MrMurman <74971676+MrMurman@users.noreply.github.com>
2023-08-08 13:11:03 +0300

    Initial Commit

---
+++ Proj-02-GuessTheFlag/Proj_02_GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Proj_02_GuessTheFlagApp.swift
+//  Proj-02-GuessTheFlag
+//
+//  Created by Андрей Бородкин on 08.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Proj_02_GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
